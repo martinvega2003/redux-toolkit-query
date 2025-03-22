4b95cea24721d6c3b3d1d4acc650bf12300d769f
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-22 16:15:09 -0300

    Import TasksList component in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import TasksList from './components/TasksList'
 
 const App = () => {
   return (
-    <div className='text-center text-6xl text-red-600'>App</div>
+    <div className='text-center text-6xl text-red-600'>
+      <TasksList />
+    </div>
   )
 }
 
