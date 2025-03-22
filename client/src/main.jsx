49861ff26a8bb39e6e254b056d6c3e1c0075bfed
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-22 16:13:01 -0300

    Import the API in main.jsx and pass it to the ApiProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ApiProvider } from "@reduxjs/toolkit/query/react"
+import { apiSlice } from './API/apiSlice.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ApiProvider api={apiSlice}>
+      <App />
+    </ApiProvider>
   </StrictMode>,
 )
