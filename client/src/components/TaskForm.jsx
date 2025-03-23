e652ba24a5259d269ccfca64892a68f2780953ed
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-23 10:33:49 -0300

    Create a basic Task Form that shows task data in console on submit

---
+++ client/src/components/TaskForm.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+const TaskForm = () => {
+
+  const [task, setTask] = useState({
+    title: '',
+    description: '',
+    completed: false,
+  })
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    console.log(task)
+  }
+
+  const handleChange = e => {
+    if (e.target.name == "completed") {
+      setTask({
+        ...task,
+        [e.target.name]: true,
+      })
+    } else {
+      setTask({
+        ...task,
+        [e.target.name]: e.target.value,
+      })
+    }
+  }
+
+  return (
+    <form>
+
+      <input type='text' name='title' onChange={handleChange} />
+      <input type='text' name='description' onChange={handleChange} />
+      <input type='checkbox' name='completed' onChange={handleChange} />
+      <button onClick={handleSubmit}>Add Task</button>
+
+    </form>
+  )
+}
+
+export default TaskForm
