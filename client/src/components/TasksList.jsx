b69fda4f8aac6644e078ff466eaab07a1134e975
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-23 11:13:41 -0300

    Update Tasks list to add delete tasks function

--- client/src/components/TasksList.jsx
+++ client/src/components/TasksList.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
-import { useGetTasksQuery } from '../API/apiSlice.js'
+import { useGetTasksQuery, useDeleteTaskMutation } from '../API/apiSlice.js'
 
 const TasksList = () => {
 
   const {data: tasks, isError, isLoading, error } = useGetTasksQuery(); // get tasks hook already come with these values.
+  const [deleteTask] = useDeleteTaskMutation();
   
   console.log(tasks)
   if (isLoading) return <div className="">Loading...</div>
   else if (isError) return <div className="">Error: {error.message}</div>
+
   return (
     <ul>
       {
-        tasks.map(task => {
+        tasks.map(task => (
           <li key={task.id}>
             <h3>{task.name}</h3>
             <p>{task.description}</p>
+            <button onClick={() => deleteTask(task.id)}>Delete</button>
+            <input type="checkbox" id={task.id} />
+            <label htmlFor={task.id}>Completed</label>
           </li>
-        })
+        ))
       }
     </ul>
   )
