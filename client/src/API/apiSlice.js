8810a5ae1d83e85253a5438a8de65afe78e5dd07
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-23 10:55:06 -0300

    Execute the getTasks function again once the createTask is executed

--- client/src/API/apiSlice.js
+++ client/src/API/apiSlice.js
@@ -7,18 +7,20 @@ export const apiSlice = createApi({
   }),
   endpoints: (builder) => ({ // Need to pass builder
     getTasks: builder.query({ 
-      query: () => '/tasks' //baseUrl + /tasks = endpoint to get all tasks
+      query: () => '/tasks', //baseUrl + /tasks = endpoint to get all tasks
+      providesTags: ["tasks"] // Name as 'tasks' to execute in other functions
     }),
     createTask: builder.mutation({ // query is used to get data, mutation is used to mutate data (In this case, create)
       query: (newTask) => ({ // Pass as an argument the new task we want to create
         url: "/tasks",
         method: "POST", // Need to specify the method
         body: newTask, // Need to pass a body
-      })
+      }),
+      invalidatesTags: ["tasks"] // Execute 'tasks' function
     })
   }),
 });
 
 // from the endpoints rtk query creates hooks that contains the data, isError, isLoading, error, and other values.
 // from getTasks endpoint it created a useGetTasksQuery hook
-export const { useGetTasksQuery } = apiSlice
+export const { useGetTasksQuery, useCreateTaskMutation } = apiSlice
